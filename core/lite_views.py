fdcc87db8a3c53e682476ebcca763af39bce4b94
R1SH4BH81 <rishabhmishra.81E@gmail.com>
2026-02-04 14:06:38 +0530

    .

--- core/lite_views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def hrms_lite_home(request):
-    """Simple home view for HRMS Lite - no authentication required"""
-    return render(request, 'core/hrms_lite.html')
--- hrms/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('core.lite_urls')),
-    path('api/', include('core.api_urls')),
-    path('admin/', admin.site.urls),
-]
